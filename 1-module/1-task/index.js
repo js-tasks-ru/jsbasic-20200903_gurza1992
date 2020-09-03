a040832398300ba5519c4f7e04cd2b2cf0d73b5f
Alex <alexbelka1992@mail.ru>
2020-09-03 19:51:50 +0300

    Убрал тестовый консоль из вторй задачи

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -3,6 +3,22 @@
  * @param {number} n
  * @returns {number}
  */
+
+
+
 function factorial(n) {
-  // ваш код...
+
+  let result = 1;
+
+
+  while (n){
+
+    result *= n--
+
+  }
+
+  return result
+
 }
+
+
